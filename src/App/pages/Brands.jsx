605071fc032fab506fe405608ff5875a9ee1341f
Virginia Villalobos <virginiavt98@gmail.com>
2023-05-21 23:40:58 +0200

    photos

--- src/App/pages/Brands.jsx
+++ src/App/pages/Brands.jsx
@@ -1,12 +1,141 @@
 import React from 'react'
 import Layout from '../../layout/LAyout'
+import { Card, CardMedia, Grid, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+import verbena from '../../assets/images/verbena/verb2.jpg'
+import moodstories from '../../assets/images/moodstories/mood11.jpg'
+import celiab from '../../assets/images/celiab/celia3.jpg'
+import cosmiel from '../../assets/images/cosmiel/cos1.jpg'
+import parole from '../../assets/images/parole/parole10.jpg'
+import flaman from '../../assets/images/verbena/verb2.jpg'
+
 
 function Brands() {
   return (
     <Layout>
-    <div>
-      BRANDS
-    </div>
+     <Grid container direction='row'  display='flex'
+      spacing={2}
+       padding='10%' 
+       justifyContent="center"
+       alignItems="center"
+       minWidth='100vw'  
+       minHeight='100vh'
+       
+       sx={{
+backgroundColor:'background.main'
+       }}
+       >
+
+        <Grid item xs={12} sm={6} lg={4} 
+        sx={{
+        }}>
+            <Link to='/editorials'>
+            <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={verbena}/>
+            </Card> 
+            </Link>
+            <Typography variant='subtitle'
+            sx={{
+                color:'dark.main',
+                
+            }}>
+                VERBENA MADRID 
+            </Typography>
+           
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+        <Link to='/beauty'>
+        <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={moodstories}/>
+            </Card>
+            </Link>
+            <Typography>
+                MOODSTORIES
+            </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+            <Link to='/beauty'>
+        <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={celiab}/>
+            </Card>
+            </Link>
+            <Typography>
+                CELIAB DESIGNER
+            </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+            <Link to='/brands'>
+        <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={cosmiel}/>
+            </Card>
+            </Link>
+            <Typography>
+                COSMIEL
+            </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+            <Link to='/brands'>
+        <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={parole}/>
+            </Card>
+            </Link>
+            <Typography>
+                PAROLE ITALY
+            </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+            <Link to='/brands'>
+        <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={flaman}/>
+            </Card>
+            </Link>
+            <Typography>
+                FLAMAN ATELIER
+            </Typography>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
--- src/App/pages/Portfolio.jsx
+++ src/App/pages/Portfolio.jsx
@@ -1,15 +1,106 @@
 import React from 'react'
 import Layout from '../../layout/LAyout'
+import { Card, CardMedia, Grid, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+import editorialsimg from '../../assets/images/m2/m22.jpg'
+import beautyimg from '../../assets/images/sarazalop/sara2.jpg'
+import portraitsimg from '../../assets/images/alba/alba10.jpeg'
+import brandsimg from '../../assets/images/verbena/verb15.jpg'
+
 
 
 function Portfolio() {
   return (
     <Layout>
+ <Grid container direction='row'  display='flex'
+      spacing={2}
+       padding='10%' 
+       justifyContent="center"
+       alignItems="center"
+       minWidth='100vw'  
+       minHeight='100vh'
+       
+       sx={{
+backgroundColor:'background.main'
+       }}
+       >
 
-    
-    <div>
-      PORTFOLIO PAGE
-    </div>
+        <Grid item xs={12} sm={6} lg={3} 
+        sx={{
+        }}>
+            <Link to='/editorials'>
+            <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={editorialsimg}/>
+            </Card> 
+            </Link>
+            <Typography variant='subtitle'
+            sx={{
+                color:'dark.main',
+                
+            }}>
+                EDITORIALS
+            </Typography>
+           
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+        <Link to='/beauty'>
+        <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={beautyimg}/>
+            </Card>
+            </Link>
+            <Typography>
+                BEAUTY
+            </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+            <Link to='/beauty'>
+        <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={portraitsimg}/>
+            </Card>
+            </Link>
+            <Typography>
+                PORTRAITS
+            </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+            <Link to='/brands'>
+        <Card sx={{
+                aspectRatio: 1,
+                borderRadius:0,
+                maxHeight:'50vh'
+            }}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={brandsimg}/>
+            </Card>
+            </Link>
+            <Typography>
+                BRANDS
+            </Typography>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
